1d5830d9f7e85512a9ce76e5fce9d099780c6bf3
Colin Hoernig <Hoernig.Colin@gmail.com>
2015-11-04 23:27:47 -0600

    Implement initial routes for index and /store/:storeId with randomly generated store names.  Upgrade react-router and history packages.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,15 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+var History = ReactRouter.History;
+var createBrowserHistory = require('history/lib/createBrowserHistory');
+
+var h = require('./helpers');
+
 var App = React.createClass({
   render: function() {
     return (
@@ -51,15 +60,37 @@ var Inventory = React.createClass({
  * StorePicker
  */
 var StorePicker = React.createClass({
+  mixins: [History],
+  goToStore: function(e) {
+    e.preventDefault();
+    var storeId = this.refs.storeId.value;
+    this.history.pushState(null, '/store/' + storeId);
+  },
   render: function() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" ref="storeId" required />
+        <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
         <input type="Submit" />
       </form>
     )
   }
 });
 
-ReactDOM.render(<App />, document.querySelector('#main'));
+var NotFound = React.createClass({
+  render: function() {
+    return (
+      <h1>Not Found</h1>
+    )
+  }
+});
+
+var routes = (
+  <Router history={createBrowserHistory()}>
+    <Route path="/" component={StorePicker} />
+    <Route path="/store/:storeId" component={App} />
+    <Route path="*" component={NotFound} />
+  </Router>
+);
+
+ReactDOM.render(routes, document.querySelector('#main'));
